1e7889f143d9e1e765cfdc8084e405d1d18dc56f
vinizin-teste <chayaneortiz84@gmail.com>
2025-09-22 16:52:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "0.0.0.0",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  base: "./", // 👈 garante que os assets sejam servidos corretamente
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "./", // 🔑 ESSENCIAL para funcionar na Hostinger
-}));
+});
